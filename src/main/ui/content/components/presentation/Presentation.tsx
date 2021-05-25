f71eb61f821f497ee38d74850edf0a9062db24bd
PavelMalta <shamashov87@mail.ru>
2021-05-25 20:20:29 +0300

    add super components

--- src/main/ui/content/components/presentation/Presentation.tsx
+++ src/main/ui/content/components/presentation/Presentation.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import SuperInputText from "../../../common/c1-SuperInputText/SuperInputText";
+import SuperButton from "../../../common/c2-SuperButton/SuperButton";
+import SuperCheckbox from "../../../common/c3-SuperCheckbox/SuperCheckbox";
 
 
 export const Presentation = () => {
     return (
         <div>
-            Presentation
+            <SuperInputText/>
+            <SuperButton/>
+            <SuperCheckbox/>
         </div>
     )
 }
