25514ce53731a83a8d122fe9c8cdce9f8f8dcf21
PavelMalta <shamashov87@mail.ru>
2021-05-25 17:54:35 +0300

    add routes.ts

---
+++ src/main/router/routes.ts
@@ -0,0 +1,10 @@
+
+export const routes = {
+    profile: "/",
+    login: "/login",
+    registration: "/registration",
+    notFound: "/404",
+    passwordRecover: "/recover",
+    newPassword: "/newPassword",
+    presentation: "/presentation",
+}
