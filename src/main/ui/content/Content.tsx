27d2ccc22199c0860373df2e527787e0cf668dff
TatsianaSamtsova <tanyaminsk92@gmail.com>
2021-05-29 16:16:46 +0300

    LoginPage

--- src/main/ui/content/Content.tsx
+++ src/main/ui/content/Content.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import {Route, Switch} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import s from "./Content.module.css"
 import {routes} from "../../router/routes";
-import {Login} from "./components/login/Login";
 import {Registration} from "./components/registration/Registration";
 import {Profile} from "./components/profile/Profile";
 import {NotFound} from "./components/notFound/NotFound";
-import { PasswordRecover } from "./components/passwordRecover/PasswordRecover";
+import {PasswordRecover} from "./components/passwordRecover/PasswordRecover";
 import {NewPassword} from "./components/newPassword/NewPassword";
 import {Presentation} from "./components/presentation/Presentation";
+import {LoginPage} from "./components/login/Login";
 
 export const Content = () => {
     return (
         <div className={s.contentContainer}>
             <Switch>
-                <Route exact path={routes.login} render={() => <Login/>} />
+                <Route path={'/'} exact render={() => <Redirect to={routes.login}/>}/>
+                <Route exact path={routes.login} render={() => <LoginPage/>} />
                 <Route exact path={routes.registration} render={() => <Registration/>} />
                 <Route exact path={routes.profile} render={() => <Profile/>} />
                 <Route exact path={routes.passwordRecover} render={() => <PasswordRecover/>} />
