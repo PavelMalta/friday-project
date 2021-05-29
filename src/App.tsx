ddb5fc92f6751143cfc164a1c55d830959427e45
PavelSuchkov <suchkovp@gmail.com>
2021-05-29 18:23:23 +0300

    add Registration.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import s from './App.module.css';
 import { Content } from './main/ui/content/Content';
 import { Header } from './main/ui/header/Header';
 import { Navigation } from './main/ui/navigation/Navigation';
+import {Registration} from "./main/ui/content/components/registration/Registration";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Header/>
             <Navigation/>
             <Content/>
+            <Registration/>
         </div>
     );
 }
