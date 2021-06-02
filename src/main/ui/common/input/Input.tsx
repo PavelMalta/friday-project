391251eee74032e20db7419909edebd9d3c151a5
TatsianaSamtsova <tanyaminsk92@gmail.com>
2021-06-02 17:40:07 +0300

    mail input without eye

--- src/main/ui/common/input/Input.tsx
+++ src/main/ui/common/input/Input.tsx
@@ -37,7 +37,6 @@ export const Input = (props: InputPropsType) => {
         return false;
     }
 
-
     return (
         <form className={s.wrapper} style= {props.style}>
             <label className={s.label}>{props.title}</label>
@@ -52,9 +51,11 @@ export const Input = (props: InputPropsType) => {
 
              {/*Глазик чекбокс*/}
             <div className={s.checkbox}>
+                <div >{props.idName ==='inputPassword'
+                    ? <><input className={s.checkboxInput} type="checkbox" onChange={togglePassword} id={props.id}/>
+                    <label className={s.checkboxLable} htmlFor={props.htmlFor}></label></>
+                     : null}</div>
 
-                <input className={s.checkboxInput} type="checkbox" onChange={togglePassword} id={props.id}/>
-                <label className={s.checkboxLable} htmlFor={props.htmlFor}></label>
             </div>
 
 
