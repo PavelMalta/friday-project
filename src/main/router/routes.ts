c8f2e7b2e0f47651835328b33f31fd09c5332380
PavelMalta <shamashov87@mail.ru>
2021-06-04 19:45:42 +0300

    fixed message for api-passwordRecover.ts, routes.ts

--- src/main/router/routes.ts
+++ src/main/router/routes.ts
@@ -5,6 +5,6 @@ export const routes = {
     registration: "/registration",
     notFound: "/404",
     passwordRecover: "/recover",
-    newPassword: "/newPassword",
+    newPassword: "/newPassword/:token",
     presentation: "/presentation",
 }
