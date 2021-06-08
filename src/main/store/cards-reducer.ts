c620ffed26054098b34ecbdd4ac4f1a137ddb5c3
PavelMalta <shamashov87@mail.ru>
2021-06-08 14:49:55 +0300

    add getPacksTC

--- src/main/store/cards-reducer.ts
+++ src/main/store/cards-reducer.ts
@@ -1,4 +1,7 @@
 import {CardsResponseType} from "../ui/content/components/cards/cardsTable/api-cards";
+import {packsAPI, PacksQueryParamsType} from "../ui/content/components/packs/api-packs";
+import {Dispatch} from "redux";
+import {getPacksAC} from "./packs-reducer";
 
 const initialState = {
      cardsTableData: {
@@ -19,11 +22,33 @@ type InitialStateType = {
 
 export const newPasswordReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
     switch (action.type) {
+        case "GET-CARDS":
+            return {...state, cardsTableData: action.cardsTableData}
+        case "IS-FETCHING":
+            return {...state, isFetching: action.isFetching}
         default:
             return state
     }
 }
 
-type ActionType = {
-    type: string
+const getCardsAC = (cardsTableData: CardsResponseType) => {
+    return {type: "GET-CARDS", cardsTableData} as const
 }
+const isFetchingAC = (isFetching: boolean) => ({type: "IS-FETCHING", isFetching} as const)
+
+
+export const getPacksTC = (packQueryParams: PacksQueryParamsType) => (dispatch: Dispatch<ActionType>) => {
+    dispatch(isFetchingAC(true))
+    packsAPI.getPacks(packQueryParams)
+        .then(res => {
+            dispatch(getCardsAC(res.data))
+        })
+        .catch((e) => {
+
+        })
+        .finally(() => {
+            dispatch(isFetchingAC(false))
+        })
+}
+
+type ActionType = ReturnType<typeof getCardsAC> | ReturnType<typeof isFetchingAC>
--- src/main/ui/content/components/cards/Cards.tsx
+++ src/main/ui/content/components/cards/Cards.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 
 export const Cards = () => {
+
+
+
     return (
         <div>
 
