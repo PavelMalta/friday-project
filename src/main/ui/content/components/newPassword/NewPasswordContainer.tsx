6c324cc57540c27b4e70ccfe3adbed5413a3c30d
TatsianaSamtsova <tanyaminsk92@gmail.com>
2021-06-10 23:04:41 +0300

    New password

--- src/main/ui/content/components/newPassword/NewPasswordContainer.tsx
+++ src/main/ui/content/components/newPassword/NewPasswordContainer.tsx
@@ -5,17 +5,22 @@ import {AppRootStateType} from "../../../../store/store";
 import {NewPassword} from "./NewPassword";
 import {Redirect, useParams} from "react-router-dom";
 import {resetPasswordTC} from "../../../../store/newPassword-reducer";
+import s from "./NewPassword.module.scss";
+import {Logo} from "../../../common/logo/Logo";
+import {TitleH2} from "../../../common/titleh2/TitleH2";
+import {Input} from "../../../common/input/Input";
+import {Button} from "../../../common/button/Button";
 
 
 export const NewPasswordContainer = () => {
 
-
+    const {token} = useParams<{ token: string }>();
     const [password, setPassword] = useState("")
     const isFetching = useSelector<AppRootStateType, boolean>((state) => state.newPassword.isFetching)
     const error = useSelector<AppRootStateType, string | null>((state) => state.newPassword.error)
     const success = useSelector<AppRootStateType, boolean>((state) => state.newPassword.success)
     const dispatch = useDispatch();
-    const {resetPasswordToken} = useParams<{ resetPasswordToken: string }>();
+
 
     const onChangePassword = (password: string) => {
         setPassword(password)
@@ -30,7 +35,8 @@ export const NewPasswordContainer = () => {
         }  else if (password.length < 8) {
             dispatch(setErrorAC('Password must be more 7 symbols'))
         } else {
-            dispatch(resetPasswordTC(password, resetPasswordToken))
+            debugger
+            dispatch(resetPasswordTC(password, token))
         }
     }
 
@@ -46,12 +52,41 @@ export const NewPasswordContainer = () => {
     }
 
     return (
-        <NewPassword     password={password}
-                         error={error}
-                         onChangePassword={onChangePassword}
-                         onclickHandler={onclickHandler}
-                         isFetching={isFetching}
-        />
+        <div className={s.newPassword}>
+            <div className={s.wrapper}>
+                <div className={s.inner}>
+                    <Logo/>
+                    <TitleH2 value="Create new password"/>
+                    <div className={s.form}>
+                        <Input title="New password"
+                               type="password"
+                               name="password"
+                               value={password}
+                               onChange={onChangePassword}
+                               error={error}
+                        /> {/*Нужно убрать глазик (логика)*/}
+                    </div>
+                    <span className={s.text}>Create new password and we will send you further instructions to email </span>
+                    <div className={s.btn}>
+                        {isFetching
+                            ? <span>Loading...</span>
+                            : <Button value="Create new password"
+                                      onClick={onclickHandler}
+                            />}
+                    </div>
+                    <div className={s.singUp}>
+                        <a className={s.link} href="#">Did you remember your password?</a>
+                        <a className={s.linkSingUp} href="#">Try logging in</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        // <NewPassword     password={password}
+        //                  error={error}
+        //                  onChangePassword={onChangePassword}
+        //                  onclickHandler={onclickHandler}
+        //                  isFetching={isFetching}
+        // />
     )
 }
 
