6b8365654f6007c53d6afa7abc93f1025f7eee11
PavelMalta <shamashov87@mail.ru>
2021-06-18 16:46:19 +0300

    update App component

--- src/App.tsx
+++ src/App.tsx
@@ -23,19 +23,11 @@ function App() {
 
             <Head/>
             <div className={s.content}>
-                {/* <Content/> */}
+                 <Content/>
 
             {/* <ProfileIvan/> */}
             {/* <ProfilePeter/> */}
             {/* <EditProfile/> */}
-            {/* <Question packName={"packName"} question={"card.question"} showAnswer={() => {}}/> */}
-            <QuestionAnswer  packName={"packName"}
-                            question={"card.question"}
-                            answer={"card.answer Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."}
-                            nextHandler={() => {}}
-                            changeInputValue={() => {}}
-                            disabledButton={false}
-            />
 
             </div>
             {/* <Error/> */}
